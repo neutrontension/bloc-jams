0289178d5cb9e2eb7929d82c95c4e608d8cd3389
Roxie <Roxie@gigabyte.local>
2017-09-25 03:23:43 -0700

    assignment 11 completed

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,23 @@ var albumMarconi = {
     ]
 };
 
+// Assignment Example Album
+var albumTheWall = {
+    title: 'The Wall',
+    artist: 'Pink Floyd',
+    label: 'Columbia Records',
+    year: '1979',
+    albumArtUrl: 'assets/images/album_covers/22.jpg',
+    songs: [
+        { title: 'In the Flesh', duration: '3:16' },
+        { title: 'The Thin Ice', duration: '2:27' },
+        { title: 'Another Brick in the Wall (Part 1)', duration: '3:21'},
+        { title: 'The Happiest Days of Our Lives', duration: '1:46' },
+        { title: 'Another Brick in the Wall (Part 2)', duration: '3:59' },
+        { title: 'Mother', duration: '5:32'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
@@ -42,14 +59,13 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
-var setCurrentAlbum = function(album) {
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
+var setCurrentAlbum = function(album) {
     // #2
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -67,4 +83,14 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    var albums = [albumPicasso, albumMarconi, albumTheWall];
+    var index = 0;
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {
+          index = 0;
+        }
+    });
 };

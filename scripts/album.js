f32649d9ea93e7c34d1006d3aa7919a4dc13356f
Roxie <Roxie@gigabyte.local>
2017-09-25 21:14:01 -0700

    assignment 13 DOM scripting

--- scripts/album.js
+++ scripts/album.js
@@ -66,7 +66,11 @@ var setCurrentAlbum = function(album) {
 };
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
+    if (currentParent === null) {
+        console.log("No parent found");
+        } else if (currentParent.className === null) {
+        console.log("No parent found with that class name");
+        } else if (element) {
         var currentParent = element.parentElement;
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;

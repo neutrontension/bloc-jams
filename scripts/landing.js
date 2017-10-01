32739d12fec4c6c85f9fe46aad96ca75cc095511
Roxie <Roxie@gigabyte.local>
2017-10-01 08:24:58 -0700

    completed checkpoint 21

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,33 @@
-// #5 dom selector that gets .point elements no longer needed because of #7
- var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point'); //getElementsByClassName - point class
 
-   var revealPoint = function() {
-       // #7
-       $(this).css({
-           opacity: 1,
-           transform: 'scaleX(1) translateY(0)'
-       });
-    };
-    // #6 
-    $.each($('.point'), revealPoint);
+var revealAllPoints = function(point){ //sets opacity, transform. Takes in point argument to point out which point is it changing its opacity and gets transformed
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform="scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
 
-$(window).load(function() {
-    // #1 Object's height. Since we pass no arguments to the function, we get the height
-    if ($(window).height() > 950) {
-        animatePoints();
-    }
-
-    // #2 Searches for .selling-points and sets object height
-    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
-    // #3 Scroll is an event listener that takes a function as an argument
-    $(window).scroll(function(event) {
-        // #4 Animates points when scroll occurs
-        if ($(window).scrollTop() >= scrollDistance) {
-            animatePoints();
-        }
+var animatePoints = function(){ //animatePoints function
+  var revealPoint = function() { //defines revealPoint(); takes self (this) and updates its CSS and transformation values.
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
     });
+  };
+  $.each($('.point'), revealPoint); //look for point class, applies revealPoint() function to each of them
+};
+//In short, animatePoints() will look for each '.point' in the array and apply revealPoint to each element
+
+$(window).load(function(event){ // originally window.onload = function(); window is the object (entire); $(window). onload = function() becomes .load(function)
+  if ($(window).height() > 950){ //if height of window is > 950px, applies animatePoints rightaway!
+    animatePoints();
+  }
+
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+  $(window).scroll(function(event){ //if scroll eventListener is active,
+    if ($(window).scrollTop() >= scrollDistance){ //and if distance is such,
+    animatePoints(); //runs this
+  }// it essentially waits for user to be withing viewing distance of the transformation!
+  });
 });
